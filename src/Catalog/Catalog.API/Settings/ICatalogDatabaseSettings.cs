5a8745b8622118c7e56c127e34ffbb4d32cedd58
dmuruges <sekar.murugesan@gmail.com>
2020-08-10 21:13:42 +0530

    ASP.NET Core Web API Settings Implementation from appsettings.json

---
+++ src/Catalog/Catalog.API/Settings/ICatalogDatabaseSettings.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson.Serialization.IdGenerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Settings
+{
+    public interface ICatalogDatabaseSettings
+    {
+        string CollectionName { get; set; }
+        string DatabaseName { get; set; }
+        string ConnectionString { get; set; }
+        
+    }
+}
